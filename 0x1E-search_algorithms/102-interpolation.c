815a82f4a4df0297241ead24a07d03dcb70c0ca2
Mohamed Zayed <mohammedkhaled077@gmail.com>
2024-04-11 07:25:02 +0200

    102-interpolation.c

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -1,42 +1,41 @@
 #include "search_algos.h"
+#include <stddef.h>
 
 /**
-  * interpolation_search - Searches for a value in a sorted array
-  *                        of integers using interpolation search.
-  * @array: A pointer to the first element of the array to search.
-  * @size: The number of elements in the array.
-  * @value: The value to search for.
-  *
-  * Return: If the value is not present or the array is NULL, -1.
-  *         else, the first index where the value is located.
-  *
-  * Description: Prints a value every time it is compared in the array..
-  */
+ * interpolation_search - function searches by interpolation a target value
+ * @array: Array (sorted) of elements to search for target value
+ * @size: Total numbers of elements in the array
+ * @value: Target value to search for
+ * Return: return -1, if value is not present in array or array is null
+ */
+
+
 int interpolation_search(int *array, size_t size, int value)
 {
-	size_t i, l, r;
+	size_t a, l, h;
 
-	if (array == NULL)
-		return (-1);
-
-	for (l = 0, r = size - 1; r >= l;)
+	l = 0;
+	h = size - 1;
+	while (h >= 1)
 	{
-		i = l + (((double)(r - l) / (array[r] - array[l])) * (value - array[l]));
-		if (i < size)
-			printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		a = l + (double)((value - array[l]) * (h - l)) / (array[h] - array[l]);
+		if (a < size)
+			printf("Value checked array[%ld] = [%d]\n", a, array[a]);
 		else
 		{
-			printf("Value checked array[%ld] is out of range\n", i);
+			printf("Value checked array[%ld] is out of range\n", a);
 			break;
 		}
 
-		if (array[i] == value)
-			return (i);
-		if (array[i] > value)
-			r = i - 1;
+		if (array[a] == value)
+			return (a);
+		else if (array[a] < value)
+			l = a + 1;
 		else
-			l = i + 1;
+			h = a - 1;
 	}
+	if (array == NULL || !array)
+		return (-1);
 
 	return (-1);
 }
