eee59191f35cf10bfa8b50be0431236f198ec698
Mohamed Zayed <mohammedkhaled077@gmail.com>
2024-04-11 07:05:25 +0200

    102-interpolation.c

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -1,42 +1,34 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "search_algos.h"
-
 /**
- * interpolation_search - function that searches for a value in a
- * sorted array of integers using the Interpolation search algorithm
- * @array: pointer to the first element
- * @size: number of elements
- * @value: value to search
- * Return: void
+ * interpolation_search - a function that searches for a value
+ * in a sorted array of integers using the interpolation search algorithm
+ * @array: pointer to the first element of the array to search in
+ * @size: is the number of elements in array
+ * @value: the value to search for
+ * Return: return the first index where value is located
  */
-
 int interpolation_search(int *array, size_t size, int value)
 {
-int low = 0, high = size - 1;
-size_t pos;
-if (array == NULL || size == 0)
-{
-return (-1);
-}
-while (low <= high && value >= array[low] && value <= array[high])
-{
-pos = low + (((double)(high - low) / (array[high] - array[low]))
-		* (value - array[low]));
-printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
-if (array[pos] == value)
-{
-return (pos);
-}
-else if (array[pos] < value)
-{
-low = pos + 1;
-}
-else
-{
-high = pos - 1;
-}
-}
-printf("Value checked array[%d] is out of range\n", high);
-return (-1);
+	uint high = 0, low = 0, pos = 0;
+
+	if (!array || size == 0)
+		return (-1);
+	high = size - 1;
+	pos = low + (((double)(high - low) /
+		      (array[high] - array[low])) * (value - array[low]));
+	if (pos > size - 1)
+		printf("Value checked array[%d] is out of range\n", pos);
+	while (high >= low && array[high] >= value && array[low] <= value)
+	{
+		printf("Value checked array[%d] = [%d]\n", pos, array[pos]);
+		if (array[pos] == value)
+			return (pos);
+		else if (array[pos] > value)
+			high = pos - 1;
+		else
+			low = pos + 1;
+		pos = low + (((double)(high - low) /
+			      (array[high] - array[low])) * (value - array[low]));
+	}
+	return (-1);
 }
